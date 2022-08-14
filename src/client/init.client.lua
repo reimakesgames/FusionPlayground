6761b7545919eb4f1a8ba74e44299611e8fdb297
= <=>
2022-08-15 00:02:10 +0800

    executor

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,7 @@
-print("Hello world, from client!")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
+
+require(ReplicatedStorage.Common.mock.twitter.index)
